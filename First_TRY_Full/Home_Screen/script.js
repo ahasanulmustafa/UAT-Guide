ef36134956d0369a96e80d7e2a320de3251c101b
AHASANUL300 <ahasan.backend@gmail.com>
2024-01-18 00:26:20 +0530

    Initial Push

---
+++ First_TRY_Full/Home_Screen/script.js
@@ -0,0 +1,15 @@
+document.getElementById("button1").onclick = function () {
+  location.href = "../synapse_asz/index.html";
+};
+
+document.getElementById("button2").onclick = function () {
+  location.href = "../synapse_non_asz/index.html";
+};
+
+document.getElementById("button3").onclick = function () {
+  location.href = "../cp4_asz/index.html";
+};
+
+document.getElementById("button4").onclick = function () {
+  location.href = "../cp4_non_asz/index.html";
+};
---
+++ First_TRY_Full/cp4_non_asz/script.js
@@ -0,0 +1,11 @@
+document.getElementById("button1").onclick = function () {
+  location.href = "initial_build/index.html";
+};
+
+document.getElementById("button2").onclick = function () {
+  location.href = "full_uat_cr/index.html";
+};
+
+document.getElementById("button3").onclick = function () {
+  location.href = "uat_execution/index.html";
+};
